95e4a2d28cc4c08bc6cb0e95eda0db65564cc7ef
Mallick199 <chinmaya.dob1999@gmail.com>
2025-09-12 22:25:40 +0530

    fix

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
-const Product = require('../models/product');
+const Product = require('../models/Product');
 const router = express.Router();
 
 // Configure multer for image uploads
